84e80a003de3ad567c7efa60ad3f421068c31d37
Stanislav Bobrov <s.bobrov@group-ib.com>
2023-05-07 15:12:18 +0300

    Initial commit

---
+++ client.go
@@ -0,0 +1,81 @@
+package mysql
+
+import (
+	"database/sql"
+	"encoding/json"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/iooojik/go-logger"
+	"time"
+)
+
+type Config struct {
+	Dsn string
+}
+
+type Client struct {
+	Connection *sql.DB
+}
+
+// NewClient создание клиента mysql
+func NewClient(cfg Config) (*Client, error) {
+	db, err := sql.Open("mysql", cfg.Dsn)
+	if err != nil {
+		return nil, err
+	}
+	db.SetConnMaxLifetime(10 * time.Second)
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	logger.LogPositive("successfully connected to mysql")
+	return &Client{
+		Connection: db,
+	}, nil
+}
+
+func (c *Client) GetStat() sql.DBStats {
+	return c.Connection.Stats()
+}
+
+func (c *Client) Query(query string, args ...any) (*sql.Rows, error) {
+	logger.LogDebug("executing", query, args)
+	return c.Connection.Query(query, args...)
+}
+
+func (c *Client) Execute(query string, args ...any) (sql.Result, error) {
+	logger.LogDebug("executing", query, args)
+	return c.Connection.Exec(query, args...)
+}
+
+func ReadRows[T any](rows *sql.Rows) ([]T, error) {
+	colNames, err := rows.Columns()
+	if err != nil {
+		return nil, err
+	}
+	cols := make([]*string, len(colNames))
+	colPtrs := make([]interface{}, len(colNames))
+	for i := 0; i < len(colNames); i++ {
+		colPtrs[i] = &cols[i]
+	}
+	items := make([]T, 0)
+	var myMap = make(map[string]any)
+	for rows.Next() {
+		scanErr := rows.Scan(colPtrs...)
+		if scanErr != nil {
+			return nil, scanErr
+		}
+		for i, col := range cols {
+			myMap[colNames[i]] = col
+		}
+		rowItem := new(T)
+		data, e := json.Marshal(myMap)
+		if e != nil {
+			return nil, e
+		}
+		e = json.Unmarshal(data, rowItem)
+		if e != nil {
+			return nil, e
+		}
+		items = append(items, *rowItem)
+	}
+	return items, nil
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module mysql_client
+
+go 1.20
+
+require (
+	github.com/go-sql-driver/mysql v1.7.1
+	github.com/iooojik/go-logger v0.0.4
+)
+
+require github.com/pkg/errors v0.9.1 // indirect
